d00017feb9f13d5f644e1d6e8d5bfaaff15e2083
Lucas-I-Marciano <lucas.marciano99@outlook.com>
2025-03-24 18:31:57 -0300

    feat: Add SideBar framework

--- frontend/src/components/SideBar/SideBar.tsx
+++ frontend/src/components/SideBar/SideBar.tsx
@@ -1,5 +1,16 @@
 import "./SideBar.css";
 
 export const SideBar = () => {
-  return <div className="sidebar"></div>;
+  return (
+    <div className="sidebar">
+      <div className="sidebar-logo"></div>
+      <div className="sidebar-content">
+        <div className="sidebar-content-section">
+          <div className="sidebar-content-section-title"></div>
+          <div className="sidebar-content-section-content"></div>
+        </div>
+      </div>
+      <div className="sidebar-content-footer"></div>
+    </div>
+  );
 };

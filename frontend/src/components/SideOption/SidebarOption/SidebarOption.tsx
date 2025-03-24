79292af6791063e298fc9c85d659b58db54b832d
Lucas-I-Marciano <lucas.marciano99@outlook.com>
2025-03-24 19:38:50 -0300

    feat: Create SidebarOption based on provided props

--- frontend/src/components/SideOption/SidebarOption/SidebarOption.tsx
+++ frontend/src/components/SideOption/SidebarOption/SidebarOption.tsx
@@ -1,14 +1,33 @@
 import "./SidebarOption.css";
 
-export const SidebarOption = () => {
+import arrowRight from "../../../assets/icons/arrow_right.png";
+import arrowDown from "../../../assets/icons/arrow_down.png";
+
+export const SidebarOption = (props: {
+  icon: string;
+  text: string;
+  arrowNeeded: boolean;
+  arrowDirection?: "right" | "left";
+}) => {
+  const { icon, text, arrowNeeded = false, arrowDirection = "right" } = props;
   return (
     <div className="sidebar-option">
       <div className="sidebar-option-content">
-        <img></img>
-        <p></p>
+        <img src={icon}></img>
+        <p>{text}</p>
       </div>
 
-      <button className="sidebar-option-button"></button>
+      {arrowNeeded ? (
+        <button className="sidebar-option-button">
+          {arrowDirection == "right" ? (
+            <img src={arrowRight}></img>
+          ) : (
+            <img src={arrowDown}></img>
+          )}
+        </button>
+      ) : (
+        <></>
+      )}
     </div>
   );
 };

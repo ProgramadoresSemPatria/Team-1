99d8d563b3d787d3109d3af853dd2663e05fc08a
Lucas-I-Marciano <lucas.marciano99@outlook.com>
2025-03-24 20:45:25 -0300

    feat: Fullfill Sidebar section content from props

--- frontend/src/components/SideBar/SideBar.tsx
+++ frontend/src/components/SideBar/SideBar.tsx
@@ -1,13 +1,19 @@
+import { ReactNode } from "react";
+import logo from "../../assets/logo.png";
 import "./SideBar.css";
 
-export const SideBar = () => {
+export const SideBar = (props: { children: ReactNode }) => {
+  const { children } = props;
   return (
     <div className="sidebar">
-      <div className="sidebar-logo"></div>
+      <div className="sidebar-logo">
+        <img className="sidebar-logo-image" src={logo}></img>
+        <span className="sidebar-logo-text">Feed AI</span>
+      </div>
       <div className="sidebar-content">
         <div className="sidebar-content-section">
-          <div className="sidebar-content-section-title"></div>
-          <div className="sidebar-content-section-content"></div>
+          <div className="sidebar-content-section-title">Quick Access</div>
+          <div className="sidebar-content-section-content">{children}</div>
         </div>
       </div>
       <div className="sidebar-content-footer"></div>

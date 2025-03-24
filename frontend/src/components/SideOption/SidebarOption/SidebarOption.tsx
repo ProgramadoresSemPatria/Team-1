bcd33df8a6696c3f454c7efbfd63cb8d1d17bad4
Lucas-I-Marciano <lucas.marciano99@outlook.com>
2025-03-24 19:37:29 -0300

    feat: Add SidebarOption framework and its css file

---
+++ frontend/src/components/SideOption/SidebarOption/SidebarOption.tsx
@@ -0,0 +1,14 @@
+import "./SidebarOption.css";
+
+export const SidebarOption = () => {
+  return (
+    <div className="sidebar-option">
+      <div className="sidebar-option-content">
+        <img></img>
+        <p></p>
+      </div>
+
+      <button className="sidebar-option-button"></button>
+    </div>
+  );
+};

6c1910dea025142afafe3920cc4fed5b61160176
Lucas-I-Marciano <lucas.marciano99@outlook.com>
2025-03-25 07:17:38 -0300

    feat: Create Header structure

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -1,3 +1,14 @@
 export const Header = () => {
-  return <div className="header">Header</div>;
+  return (
+    <div className="header">
+      <div className="header-info">
+        <div className="header-info-left"></div>
+        <div className="header-info-right"></div>
+      </div>
+      <div className="header-options">
+        <div className="header-options-left"></div>
+        <div className="header-options-right"></div>
+      </div>
+    </div>
+  );
 };

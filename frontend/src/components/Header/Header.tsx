a453e44560889f70a8ef31d51a5d0a5b283e340d
Lucas-I-Marciano <lucas.marciano99@outlook.com>
2025-03-25 07:21:09 -0300

    feat: Create Header content

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -1,13 +1,29 @@
+import logo from "../../assets/logo.png";
+
 export const Header = () => {
   return (
     <div className="header">
       <div className="header-info">
-        <div className="header-info-left"></div>
-        <div className="header-info-right"></div>
+        <div className="header-info-left">
+          <button>English</button>
+          <button>Support</button>
+        </div>
+        <div className="header-info-right">
+          <button>Blog</button>
+          <button>About us</button>
+        </div>
       </div>
       <div className="header-options">
-        <div className="header-options-left"></div>
-        <div className="header-options-right"></div>
+        <div className="header-options-left">
+          <img src={logo}></img>
+          <button>Features</button>
+          <button>Case Studies</button>
+          <button>Pricing</button>
+          <button>Applications</button>
+        </div>
+        <div className="header-options-right">
+          <button>Get Start</button>
+        </div>
       </div>
     </div>
   );

74b0770981d2424fe75f5289876517a18b0b7a4a
Lucas-I-Marciano <lucas.marciano99@outlook.com>
2025-03-25 20:41:14 -0300

    refactor: Rename auth.py to Auth.py

--- backend/api/main.py
+++ backend/api/main.py
@@ -3,7 +3,7 @@ from typing import Annotated
 
 from sqlmodel import Session
 
-from api.routers import auth, Users
+from api.routers import Auth, Users
 from api.db import create_all_table_and_db, get_session
 from api.utils.token import decode_token, protected_endpoint
 
@@ -29,7 +29,7 @@ app = FastAPI(
     }
 )
 
-app.include_router(auth.router, prefix="/api")
+app.include_router(Auth.router, prefix="/api")
 app.include_router(Users.router, prefix="/api")
 
 @app.on_event("startup")

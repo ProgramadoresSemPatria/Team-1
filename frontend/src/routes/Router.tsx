01b8f1967b73b804f707f3b61d1f4d8723591aea
Pedro Oliveira <pedrogagodev@gmail.com>
2025-03-26 14:27:35 -0300

    refactor: replace App component with LandingPage in Router for improved navigation

--- frontend/src/routes/Router.tsx
+++ frontend/src/routes/Router.tsx
@@ -1,12 +1,12 @@
 import { Route, Routes } from 'react-router-dom';
-import App from '../App';
 import { Login } from '@/pages/Login';
 import { Register } from '@/pages/Register';
+import { LandingPage } from '@/pages/LandingPage';
 
 export function Router() {
   return (
     <Routes>
-      <Route path="/" element={<App />} />
+      <Route path="/" element={<LandingPage />} />
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
     </Routes>

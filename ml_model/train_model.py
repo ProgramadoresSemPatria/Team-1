2d8a33f5a0eb41b0d2e2dc943c4e55126e08d59c
Joao araujo <joaoaraujo.vitor@hotmail.com>
2025-03-26 13:34:08 -0300

    fix def clear_text

--- ml_model/train_model.py
+++ ml_model/train_model.py
@@ -3,7 +3,7 @@ import nltk
 import re
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
-
+from nltk.tokenize import wordpunct_tokenize
 nltk.download('stopwords')
 nltk.download('punkt')
 
@@ -13,8 +13,13 @@ stop_words = set(stopwords.words('english'))
 
 def clear_text(text):
     text = text.lower()
-    text = text.sub(r'[^a-zA-Z\s]', '', text)
-    word =word_tokenize(text)
+    text = re.sub(r'[^a-zA-Z\s]', '', text)
+    word =wordpunct_tokenize(text)
     filtered_word = [p for p in word if p not in stop_words]
     return ' '.join(filtered_word)
+
+df['Text'] = df ['Text'].apply(clear_text)
+
+
+print(df.head())
  

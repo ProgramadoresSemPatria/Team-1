f30c93fa43b8681d738c0e1e7ec5c54b1a458062
Lucas-I-Marciano <lucas.marciano99@outlook.com>
2025-03-26 19:23:07 -0300

    feat: Add find feedback route

--- backend/api/routers/Search.py
+++ backend/api/routers/Search.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, File, UploadFile
+from fastapi import APIRouter, File, UploadFile, Body
 from typing import Annotated
 
 from ..enum.TagsEnum import TagsEnum
@@ -12,3 +12,8 @@ router = APIRouter(
 async def upload_file(file: Annotated[UploadFile, File()]):
     # ALL THE AI LOGIC HERE
     return {"Message":f"{file.filename} is under evalluation, please wait until finish!\nWe will let you know once finished"}
+
+@router.post('/find')
+async def find_feedback(keywords:Annotated[list[str], Body()]):
+    # ALL THE AI LOGIC HERE
+    return {"message": "We are searching for feedbacks for you, please wait until finish!", "keywords": keywords}

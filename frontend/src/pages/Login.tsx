9ffc40bd9803d0926ddbdbc1a60fbe9e427188d2
Pedro Oliveira <pedrogagodev@gmail.com>
2025-03-26 16:44:31 -0300

    feat: enhance login page layout with shadcn Card component and improved styling

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,5 +1,6 @@
 import FeedAI_Logo from '@/assets/FeedAI_Logo.png';
 import { Button } from '@/components/ui/button';
+import { Card, CardFooter } from '@/components/ui/card';
 import { Input } from '@/components/ui/input';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useForm } from 'react-hook-form';
@@ -33,19 +34,19 @@ export function Login() {
         </h1>
         <p className="my-5 text-xl">Access your account right now!</p>
       </div>
-      <div className="min-h-[60vh] flex flex-col items-center justify-start md:mt-28">
-        <h2 className="text-2xl mt-4 mb-2 text-sky-600 font-bold">
+      <Card className="flex flex-col items-center justify-start px-6 md:w-[40vh] md:p-10">
+        <h2 className="text-3xl mt-4 mb-2 text-sky-400 font-bold md:text-4xl">
           Sign in to your account
         </h2>
 
-        <form action="" onSubmit={handleSubmit(onSubmit)}>
+        <form action="" onSubmit={handleSubmit(onSubmit)} className="w-full">
           <div className="flex flex-col gap-2">
-            <label htmlFor="email" className="font-bold">
+            <label htmlFor="email" className="font-bold text-xl md:text-2xl">
               Enter your Email
             </label>
             <Input
               type="email"
-              className="border px-1"
+              className="border px-1 py-5"
               placeholder="youremail@email.com"
               {...register('email')}
             />
@@ -54,12 +55,12 @@ export function Login() {
             )}
           </div>
           <div className="flex flex-col gap-2 mt-4">
-            <label htmlFor="password" className="font-bold">
+            <label htmlFor="password" className="font-bold text-xl md:text-2xl">
               Enter your password
             </label>
             <Input
               type="password"
-              className="border"
+              className="border px-1 py-5"
               placeholder="••••••••"
               {...register('password')}
             />
@@ -70,22 +71,24 @@ export function Login() {
           <div className="flex flex-col items-center">
             <Button
               type="submit"
-              className="text-center bg-sky-300 rounded-md p-2 font-bold mt-5 mb-3 w-full"
+              className="text-center bg-sky-600 rounded-lg p-6 font-bold text-xl mt-5 mb-3 w-full hover:cursor-pointer hover:bg-sky-700 md:text-2xl"
             >
               Sign in
             </Button>
-            <p>
-              New to FeedAI?{' '}
-              <Button
-                variant="link"
-                className="hover:cursor-pointer hover:text-blue-600 text-blue-400 mx-0 px-0"
-              >
-                Create account
-              </Button>
-            </p>
+            <CardFooter>
+              <p className="text-xl md:text-2xl">
+                New to FeedAI?{' '}
+                <Button
+                  variant="link"
+                  className="hover:cursor-pointer hover:text-blue-700 text-blue-600 mx-0 px-0 text-xl md:text-2xl"
+                >
+                  Create account
+                </Button>
+              </p>
+            </CardFooter>
           </div>
         </form>
-      </div>
+      </Card>
     </div>
   );
 }

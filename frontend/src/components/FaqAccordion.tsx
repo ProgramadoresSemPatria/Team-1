361f9427e9cb708cfe0be976c60e2d21416bc773
Pedro Oliveira <pedrogagodev@gmail.com>
2025-03-26 14:50:17 -0300

    style: enhance FaqAccordion component for improved responsiveness and typography

--- frontend/src/components/FaqAccordion.tsx
+++ frontend/src/components/FaqAccordion.tsx
@@ -13,14 +13,16 @@ export function FaqAccordion({
     <Accordion
       type="single"
       collapsible
-      className="border rounded-md px-4 mx-4"
+      className="border rounded-md px-4 mx-4 md:px-8 md:mx-8"
     >
       <AccordionItem value="item-1">
         <AccordionTrigger>
-          <h3 className="text-lg font-bold text-gray-800">{title}</h3>
+          <h3 className="text-lg font-bold text-gray-800 md:text-2xl">
+            {title}
+          </h3>
         </AccordionTrigger>
         <AccordionContent>
-          <p className="text-gray-500">{content}</p>
+          <p className="text-gray-500 md:text-lg">{content}</p>
         </AccordionContent>
       </AccordionItem>
     </Accordion>

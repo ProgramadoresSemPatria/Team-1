d6f23ad4300db9bd660a6b9e446c67d9329cbda8
Pedro Oliveira <pedrogagodev@gmail.com>
2025-03-26 14:13:18 -0300

    feat: add pricing section to LandingPage with monthly and yearly plans

--- frontend/src/pages/LandingPage.tsx
+++ frontend/src/pages/LandingPage.tsx
@@ -2,7 +2,22 @@ import { Button } from '@/components/ui/button';
 import { AspectRatio } from '@/components/ui/aspect-ratio';
 import HeroImage from '@/assets/hero-image.png';
 import { BenefitCard } from '@/components/BenefitCard';
-import { BrainCircuit, Clock, DollarSign, SquareCheckBig, Users, NotebookText } from 'lucide-react';
+import {
+  BrainCircuit,
+  Clock,
+  DollarSign,
+  SquareCheckBig,
+  Users,
+  NotebookText,
+  CircleCheck,
+} from 'lucide-react';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
 export function LandingPage() {
   return (
     <div className="flex flex-col justify-center items-center text-center">
@@ -55,10 +70,73 @@ export function LandingPage() {
           />
           <BenefitCard
             icon={<NotebookText className="w-8 h-8 text-sky-700" />}
-            title="Save your reviews" 
+            title="Save your reviews"
             description="We save your reviews and you can see them in one place"
           />
         </div>
+      </div>
+      <div className="flex flex-col gap-6 my-16">
+        <h2 className="text-2xl font-bold text-gray-800">Our Pricing</h2>
+        <Card className="mx-6 border-amber-400 border-2">
+          <CardHeader>
+            <CardTitle className="text-2xl">FeedAI</CardTitle>
+            <CardDescription className="text-3xl font-bold text-black">
+              Montly
+            </CardDescription>
+            <CardDescription className="text-2xl font-bold text-amber-400">
+              $10 <span className="text-gray-500 text-sm">/ month</span>
+            </CardDescription>
+          </CardHeader>
+          <CardContent>
+            <ul className="list-disc list-inside text-left space-y-2">
+              <li className="flex items-center gap-2">
+                <CircleCheck className="w-4 h-4 text-green-400" fill="green" />
+                All the benefits of the monthly plan
+              </li>{' '}
+              <li className="flex items-center gap-2">
+                <CircleCheck className="w-4 h-4 text-green-400" fill="green" />
+                All the benefits of the monthly plan
+              </li>{' '}
+              <li className="flex items-center gap-2">
+                <CircleCheck className="w-4 h-4 text-green-400" fill="green" />
+                All the benefits of the monthly plan
+              </li>
+            </ul>
+          </CardContent>
+          <Button className="w-5/6 mx-auto mt-4 bg-amber-400 text-white">
+            Get Started
+          </Button>
+        </Card>
+        <Card className="mx-6 border-sky-700 border-2">
+          <CardHeader>
+            <CardTitle className="text-2xl">FeedAI</CardTitle>
+            <CardDescription className="text-3xl font-bold text-black">
+              Yearly
+            </CardDescription>
+            <CardDescription className="text-2xl font-bold text-sky-700">
+              $8 <span className="text-gray-500 text-sm">/ month</span>
+            </CardDescription>
+          </CardHeader>
+          <CardContent>
+            <ul className="list-disc list-inside text-left space-y-2">
+              <li className="flex items-center gap-2">
+                <CircleCheck className="w-4 h-4 text-green-400" fill="green" />
+                All the benefits of the monthly plan
+              </li>
+              <li className="flex items-center gap-2">
+                <CircleCheck className="w-4 h-4 text-green-400" fill="green" />
+                All the benefits of the monthly plan
+              </li>
+              <li className="flex items-center gap-2">
+                <CircleCheck className="w-4 h-4 text-green-400" fill="green" />
+                All the benefits of the monthly plan
+              </li>
+            </ul>
+          </CardContent>
+          <Button className="w-5/6 mx-auto mt-4 bg-sky-700 text-white">
+            Get Started
+          </Button>
+        </Card>
       </div>
     </div>
   );

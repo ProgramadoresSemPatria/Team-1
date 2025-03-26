a681afd5bc646f4ae7be12a8baf904ab5908a9d4
Pedro Oliveira <pedrogagodev@gmail.com>
2025-03-26 12:29:41 -0300

    feat: enhance LandingPage with benefits section using BenefitCard component

--- frontend/src/pages/LandingPage.tsx
+++ frontend/src/pages/LandingPage.tsx
@@ -1,7 +1,8 @@
 import { Button } from '@/components/ui/button';
 import { AspectRatio } from '@/components/ui/aspect-ratio';
 import HeroImage from '@/assets/hero-image.png';
-
+import { BenefitCard } from '@/components/BenefitCard';
+import { BrainCircuit, Clock, DollarSign, SquareCheckBig, Users, NotebookText } from 'lucide-react';
 export function LandingPage() {
   return (
     <div className="flex flex-col justify-center items-center text-center">
@@ -21,6 +22,43 @@ export function LandingPage() {
         <Button className="mt-16 w-1/2 bg-sky-700 text-white">
           Get Started
         </Button>
+      </div>
+      <div className="flex flex-col gap-6 my-16">
+        <h2 className="text-2xl font-bold text-gray-800">
+          Benefits of using our service
+        </h2>
+        <div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3">
+          <BenefitCard
+            icon={<BrainCircuit className="w-8 h-8 text-sky-700" />}
+            title="Best insights with AI"
+            description="Use the power of AI to get the best insights about your brand"
+          />
+          <BenefitCard
+            icon={<Clock className="w-8 h-8 text-sky-700" />}
+            title="Save Time"
+            description="With just a few clicks, you can get the latest reviews and feedback from your customers"
+          />
+          <BenefitCard
+            icon={<DollarSign className="w-8 h-8 text-sky-700" />}
+            title="Save Money"
+            description="Instead of hiring a team of people to do it, we do it for you in seconds and for a fraction of the cost"
+          />
+          <BenefitCard
+            icon={<Users className="w-8 h-8 text-sky-700" />}
+            title="Connect with your customers"
+            description="We connect you with your customers and you can see what they are saying about your brand"
+          />
+          <BenefitCard
+            icon={<SquareCheckBig className="w-8 h-8 text-sky-700" />}
+            title="Easy to Use and Understand"
+            description="Our platform is designed to be easy to use and understand"
+          />
+          <BenefitCard
+            icon={<NotebookText className="w-8 h-8 text-sky-700" />}
+            title="Save your reviews" 
+            description="We save your reviews and you can see them in one place"
+          />
+        </div>
       </div>
     </div>
   );

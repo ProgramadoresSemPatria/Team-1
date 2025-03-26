48bfa3541ffd394858dfd71cb5f7bd2fc3fd85cf
Joao araujo <joaoaraujo.vitor@hotmail.com>
2025-03-26 13:26:53 -0300

    feat - def clear text

--- ml_model/train_model.py
+++ ml_model/train_model.py
@@ -4,5 +4,17 @@ import re
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 
+nltk.download('stopwords')
+nltk.download('punkt')
 
 df = pd.read_csv("dataset/reduced_reviews.csv")
+
+stop_words = set(stopwords.words('english'))
+
+def clear_text(text):
+    text = text.lower()
+    text = text.sub(r'[^a-zA-Z\s]', '', text)
+    word =word_tokenize(text)
+    filtered_word = [p for p in word if p not in stop_words]
+    return ' '.join(filtered_word)
+ 

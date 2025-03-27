8b639f3ec3c7e64b3a73145d2e74a78728c8a5c3
Joao araujo <joaoaraujo.vitor@hotmail.com>
2025-03-27 12:09:39 -0300

    feat: vectorizer and transform the text data

--- ml_model/train_model.py
+++ ml_model/train_model.py
@@ -4,3 +4,12 @@ df = pd.read_csv('dataset/reduced_reviews.csv')
 
 print (df.head())
 print(df["Sentiment"].value_counts())
+
+
+from sklearn.feature_extraction.text import CountVectorizer
+
+vectorizer = CountVectorizer(max_features=5000)
+
+X = vectorizer.fit_transform(df["Text"])
+
+y = df["Sentiment"]

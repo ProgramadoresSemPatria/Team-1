657f495285305d33c9a30f0b1a83130e3d4d533b
Joao araujo <joaoaraujo.vitor@hotmail.com>
2025-03-27 12:15:00 -0300

    feat : train model and predict test set

--- ml_model/train_model.py
+++ ml_model/train_model.py
@@ -22,3 +22,9 @@ y = df["Sentiment"]
 X_train, X_test , y_train, y_test = train_test_split(
     X, y, test_size= 0.3, random_state=42
 )
+
+model = MultinomialNB()
+model.fit(X_train,y_train)
+
+y_pred = model.predict(X_test)
+

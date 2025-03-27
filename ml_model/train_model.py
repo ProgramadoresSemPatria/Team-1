5afe15fe7821e7bd6b99a7754a23cfb18e259cee
Joao araujo <joaoaraujo.vitor@hotmail.com>
2025-03-27 14:09:53 -0300

    import joblib and os

--- ml_model/train_model.py
+++ ml_model/train_model.py
@@ -3,17 +3,15 @@ from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.metrics import classification_report
 from sklearn.feature_extraction.text import CountVectorizer
-
-
+import joblib
+import os 
 df = pd.read_csv('dataset/reduced_reviews.csv')
-
 
 vectorizer = CountVectorizer(max_features=5000)
 
 X = vectorizer.fit_transform(df["Text"])
 
 y = df["Sentiment"]
-
 
 X_train, X_test , y_train, y_test = train_test_split(
     X, y, test_size= 0.3, random_state=42

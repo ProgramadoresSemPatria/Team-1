f35f721a7684f1138142d0711c289af8d1f08a6e
Joao araujo <joaoaraujo.vitor@hotmail.com>
2025-03-27 14:22:56 -0300

    feat - app route predict csv

--- backend/app.py
+++ backend/app.py
@@ -9,3 +9,8 @@ app = Flask(__name__)
 model = joblib.load('ml_model/ml_model/model/modelo_sentimento.pkl')
 vectorizer = joblib.load('ml_model/ml_model/model/vectorizer.pkl')
 
+@app.route('predict-csv', methods=['POST'])
+def predict_from_csv():
+    file = request.files['file']
+    df = pd.read_csv(file)
+    

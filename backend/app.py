9d85ef6798f443d61bbf7f1ce1942444554c7d4b
Joao araujo <joaoaraujo.vitor@hotmail.com>
2025-03-27 14:21:05 -0300

    feat : load the model app.py

--- backend/app.py
+++ backend/app.py
@@ -6,3 +6,6 @@ from ml_model.preprocess import clear_text
 
 app = Flask(__name__)
 
+model = joblib.load('ml_model/ml_model/model/modelo_sentimento.pkl')
+vectorizer = joblib.load('ml_model/ml_model/model/vectorizer.pkl')
+

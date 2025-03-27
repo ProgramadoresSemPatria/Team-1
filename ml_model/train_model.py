5b14dcaa211717b043e48253fc92aca43a84e6af
Joao araujo <joaoaraujo.vitor@hotmail.com>
2025-03-27 12:13:56 -0300

    feat : split into train and test sets

--- ml_model/train_model.py
+++ ml_model/train_model.py
@@ -1,4 +1,8 @@
 import pandas as pd 
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.metrics import classification_report
+
 
 df = pd.read_csv('dataset/reduced_reviews.csv')
 
@@ -13,3 +17,8 @@ vectorizer = CountVectorizer(max_features=5000)
 X = vectorizer.fit_transform(df["Text"])
 
 y = df["Sentiment"]
+
+
+X_train, X_test , y_train, y_test = train_test_split(
+    X, y, test_size= 0.3, random_state=42
+)

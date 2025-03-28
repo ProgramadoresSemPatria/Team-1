0cc59a886923d17820ef5071078e5e203a8a5d81
Pedro Oliveira <pedrogagodev@gmail.com>
2025-03-28 12:13:48 -0300

    refactor: enhance WelcomeMessageSignUp component with improved layout and responsive design for better user experience

--- frontend/src/components/WelcomeMessageSignUp.tsx
+++ frontend/src/components/WelcomeMessageSignUp.tsx
@@ -8,15 +8,19 @@ export function WelcomeMessageSignUp() {
       animate={{ opacity: 1, y: 0 }}
       transition={{ duration: 0.5 }}
     >
-      <img
-        src={FeedAI_Logo}
-        alt="FeedAI logo"
-        className="h-40 w-40 mx-auto"
-      />
-      <h1 className="mt-10 text-5xl whitespace-nowrap text-center">
-        Hello, <span className="text-sky-400">Welcome!</span>
-      </h1>
-      <p className="my-5 text-xl text-center">Create your account right now!</p>
+      <div className="flex flex-col items-center justify-center w-screen lg:w-[40vh]">
+        <img
+          src={FeedAI_Logo}
+          alt="FeedAI logo"
+          className="h-40 w-40 mx-auto lg:h-60 lg:w-60"
+        />
+        <h1 className="mt-10 text-5xl whitespace-nowrap text-center lg:text-6xl">
+          Hello, <span className="text-sky-400">Welcome!</span>
+        </h1>
+        <p className="my-5 text-xl text-center lg:text-2xl">
+          Create your account right now!
+        </p>
+      </div>
     </motion.div>
   );
 }

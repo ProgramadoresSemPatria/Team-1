2e51dae9cd9ca22483e1b8bd7f40f5fd995bca58
Pedro Oliveira <pedrogagodev@gmail.com>
2025-03-28 12:13:40 -0300

    refactor: improve Stepper component layout and styling for better responsiveness and user experience

--- frontend/src/components/Stepper.tsx
+++ frontend/src/components/Stepper.tsx
@@ -41,13 +41,13 @@ export function Stepper({ steps, initialStep = 0 }: IStepperProps) {
 
   return (
     <StepperContext.Provider value={{ previousStep, nextStep, direction }}>
-      <div className="flex flex-col items-center justify-center gap-10">
-        <ul className="flex items-center justify-center gap-20 relative">
+      <div className="flex flex-col items-start justify-center gap-10">
+        <ul className="flex items-center justify-center gap-0 relative lg:gap-10">
           {steps.map((step, index) => (
             <motion.li
               key={step.label}
               className={cn(
-                'relative inline-block text-sm p-2 rounded-lg overflow-hidden',
+                'relative inline-block text-sm p-2 rounded-lg overflow-hidden lg:text-lg text-center',
                 index === currentStep && 'text-primary-foreground',
               )}
             >
@@ -79,7 +79,7 @@ export function Stepper({ steps, initialStep = 0 }: IStepperProps) {
                 )}
               </AnimatePresence>
 
-              <span className="relative z-10">
+              <span className="relative z-10 font-bold lg:text-xl">
                 {String(index + 1).padStart(2, '0')}. {step.label}
               </span>
             </motion.li>
@@ -94,7 +94,7 @@ export function Stepper({ steps, initialStep = 0 }: IStepperProps) {
 
 export const StepperFooter = ({ children }: { children: React.ReactNode }) => {
   return (
-    <footer className="flex flex-col items-center justify-center gap-2 mt-6">
+    <footer className="flex flex-col items-center justify-center w-full gap-2 mt-6">
       {children}
     </footer>
   );
@@ -103,12 +103,19 @@ export function StepperNextButton({
   size = 'default',
   type = 'button',
   onClick,
+  text,
   ...props
-}: React.ComponentPropsWithoutRef<typeof Button>) {
+}: React.ComponentPropsWithoutRef<typeof Button> & { text?: string }) {
   const { nextStep } = useStepper();
   return (
-    <Button type={type} size={size} onClick={onClick ?? nextStep} {...props}>
-      Next
+    <Button
+      type={type}
+      size={size}
+      onClick={onClick ?? nextStep}
+      {...props}
+      className="w-full bg-sky-700 font-bold text-lg lg:text-2xl lg:h-12"
+    >
+      {text ?? 'Next'}
     </Button>
   );
 }
@@ -126,6 +133,7 @@ export function StepperPreviousButton({
       size={size}
       onClick={onClick ?? previousStep}
       {...props}
+      className="w-full text-lg lg:text-2xl lg:h-12"
     >
       Previous
     </Button>

d88c487250fee49ec027e2c2d4b8ad2d41d1c291
Lucas-I-Marciano <lucas.marciano99@outlook.com>
2025-03-28 19:52:54 -0300

    refactor: Simplify SQL WHERE clause creation with helper function

---
+++ backend/api/utils/query_helper.py
@@ -0,0 +1,18 @@
+#Esse arquivo foi criado com o auxílio de IA
+from api.utils.operators import convert_text_to_operator
+
+def build_where_clause(**filters):
+    where_clause_parts = []
+
+    for column, value in filters.items():
+        if isinstance(value, dict):
+            operator = value.get("operator")
+            val = value.get("value")
+            if operator and val:
+                where_clause_parts.append(f"{column} {convert_text_to_operator(operator)} '{val}'")
+        else:
+            where_clause_parts.append(f"{column} = '{value}'")
+
+    where_clause = "WHERE " + " AND ".join(where_clause_parts) if where_clause_parts else ""
+
+    return where_clause

378e3955548bfd559b33cbdb754f8c7a0b5417dc
Gabriel <bielrj170@gmail.com>
2025-03-27 22:48:35 -0300

    feat: insert new routes and auth guard

--- frontend/src/routes/Router.tsx
+++ frontend/src/routes/Router.tsx
@@ -2,13 +2,23 @@ import { Route, Routes } from 'react-router-dom';
 import App from '../App';
 import { Login } from '@/pages/Login';
 import { Register } from '@/pages/Register';
+import { AuthGuard } from '@/layouts/AuthGuard';
+import { Upload } from '@/pages/Upload';
+import AuthProvider from '@/context';
 
 export function Router() {
   return (
-    <Routes>
-      <Route path="/" element={<App />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/register" element={<Register />} />
-    </Routes>
+    <AuthProvider>
+      <Routes>
+        <Route element={<AuthGuard isPrivate={false} />}>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Route>
+        <Route element={<AuthGuard isPrivate={true} />}>
+          <Route path="/" element={<App />} />
+          <Route path="/upload" element={<Upload />} />
+        </Route>
+      </Routes>
+    </AuthProvider>
   );
 }

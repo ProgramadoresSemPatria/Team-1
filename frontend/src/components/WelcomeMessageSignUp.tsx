b6ff872e049b51fb7b38af551a0f76492ffbdde3
Pedro Oliveira <pedrogagodev@gmail.com>
2025-03-28 02:33:00 -0300

    refactor: enhance WelcomeMessageSignUp component with animation and center alignment for improved user experience

--- frontend/src/components/WelcomeMessageSignUp.tsx
+++ frontend/src/components/WelcomeMessageSignUp.tsx
@@ -1,13 +1,22 @@
 import FeedAI_Logo from '@/assets/FeedAI_Logo.png';
+import { motion } from 'motion/react';
 
 export function WelcomeMessageSignUp() {
   return (
-    <>
-      <img src={FeedAI_Logo} alt="FeedAI logo" className="h-40 w-40" />
-      <h1 className="mt-10 text-5xl whitespace-nowrap">
+    <motion.div
+      initial={{ opacity: 0, y: 100 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.5 }}
+    >
+      <img
+        src={FeedAI_Logo}
+        alt="FeedAI logo"
+        className="h-40 w-40 mx-auto"
+      />
+      <h1 className="mt-10 text-5xl whitespace-nowrap text-center">
         Hello, <span className="text-sky-400">Welcome!</span>
       </h1>
-      <p className="my-5 text-xl">Create your account right now!</p>
-    </>
+      <p className="my-5 text-xl text-center">Create your account right now!</p>
+    </motion.div>
   );
 }

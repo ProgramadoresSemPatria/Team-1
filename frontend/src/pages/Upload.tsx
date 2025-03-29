04e30c354d30d0b8869cad888a1e67681170eb0e
Gabriel <bielrj170@gmail.com>
2025-03-29 20:24:50 -0300

    refactor: refactor upload page

--- frontend/src/pages/Upload.tsx
+++ frontend/src/pages/Upload.tsx
@@ -1,25 +1,17 @@
-import { AsideNav } from "@/components/AsideNav";
 import TriangleUpload from "@/components/TriangleUpload";
-import { Header } from "@/layouts/Header";
 
 export function Upload() {
-  return (
-    <div className="flex flex-col h-screen">
-      <Header />
-      <div className="flex flex-1 overflow-hidden pl-20">
-        <AsideNav />
-        <div className="flex-1 overflow-y-auto p-8">
-          <div className="flex flex-col gap-2">
-            <h2 className="text-3xl font-bold">Data Upload</h2>
-            <p className="text-2xl font-regular text-gray-500">
-              Upload your data file to generate insights with power of AI
-            </p>
-          </div>
-          <div className="w-full flex items-center justify-center h-[calc(100vh-300px)]">
-            <TriangleUpload />
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<>
+			<div className="flex flex-col gap-2">
+				<h2 className="text-3xl font-bold">Data Upload</h2>
+				<p className="text-2xl font-regular text-gray-500">
+					Upload your data file to generate insights with power of AI
+				</p>
+			</div>
+			<div className="w-full flex items-center justify-center h-[calc(100vh-300px)]">
+				<TriangleUpload />
+			</div>
+		</>
+	);
 }

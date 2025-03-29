62ef4e1a887a856ddc2cdd42860da125676bf2d1
Lucas-I-Marciano <lucas.marciano99@outlook.com>
2025-03-29 13:45:00 -0300

    feat: Add related_key column for foreign key relationship instead of key

--- backend/api/db/AIResponseTags.py
+++ backend/api/db/AIResponseTags.py
@@ -7,4 +7,5 @@ class AiResponseTags(SQLModel, table=True):
     tag: str|None = Field(default=None, index=True)
     consulted_query_date: datetime.datetime = Field(index=True)
     user_id: uuid.UUID
+    related_key:str|None = Field(default=None)
     key:str|None = Field(default=None, primary_key=True)

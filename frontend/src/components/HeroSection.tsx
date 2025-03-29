e9e582992264070cf5057d2fcd7dd67a07f7e4c4
Pedro Oliveira <pedrogagodev@gmail.com>
2025-03-29 15:16:57 -0300

    feat: enhance HeroSection with animated transitions and WordCascade for dynamic text display

--- frontend/src/components/HeroSection.tsx
+++ frontend/src/components/HeroSection.tsx
@@ -2,22 +2,33 @@ import { AspectRatio } from '@/components/ui/aspect-ratio';
 import { Button } from '@/components/ui/button';
 import HeroImage from '@/assets/hero-image.png';
 import { ArrowRight } from 'lucide-react';
+import { motion } from 'framer-motion';
+import { WordCascade } from './WordCascade';
 
 // TODO: Fix responsive in AspectRatio component
 
 export function HeroSection() {
   return (
-    <div className="bg-gray-100 flex flex-col items-center justify-center lg:items-start lg:text-start lg:w-full lg:max-h-[70vh] lg:px-40 lg:flex-row">
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 0.5 }}
+      className="bg-gray-100 flex flex-col items-center justify-center lg:items-start lg:text-start lg:w-full lg:max-h-[70vh] lg:px-40 lg:flex-row"
+    >
       <div className="flex flex-col">
         <h1 className="font-bold text-4xl mt-16 mb-5 md:text-6xl">
           Understand customer feedback{' '}
-          <span className="font-bold text-sky-700">in seconds.</span>
+          <WordCascade
+            words={['in seconds', 'very fast', 'at once', 'quickly', 'shortly']}
+            className="flex items-center justify-center lg:justify-start mt-4 font-bold text-sky-700"
+          />
         </h1>
+
         <p className="text-2xl text-gray-500 mt-8 lg:mt-16 md:text-2xl md:max-w-5xl">
           FeedAI analyzes your customer reviews instantly. No more dedicating
           teams for weeks to understand what people are saying about your brand.
         </p>
-        <Button className="hidden lg:flex mt-32 mb-10 h-16 rounded-xl bg-sky-700 font-bold text-2xl text-white w-8/9 hover:bg-sky-800 hover:cursor-pointer">
+        <Button className="shadow-lg shadow-black/80 hidden mt-32 mb-10 h-16 rounded-xl bg-sky-700 font-bold text-2xl text-white w-8/9 hover:scale-105 hover:bg-sky-800 hover:cursor-pointer lg:flex">
           Get Started{' '}
           <ArrowRight className="ml-2 mt-[1px] min-w-8 min-h-8 font-bold" />
         </Button>
@@ -31,10 +42,10 @@ export function HeroSection() {
           <img src={HeroImage} alt="Landing Page" className="rounded-lg" />
         </AspectRatio>
 
-        <Button className="mt-16 mb-10 w-10/11 h-12 py-7 bg-sky-700 font-bold text-lg text-white lg:hidden">
+        <Button className="shadow-lg shadow-black/80 mt-16 mb-10 w-10/11 h-12 py-7 bg-sky-700 font-bold text-lg text-white lg:hidden hover:scale-105 hover:bg-sky-800">
           Get Started <ArrowRight className="w-5 h-5 font-bold mt-0.5" />
         </Button>
       </div>
-    </div>
+    </motion.div>
   );
 }

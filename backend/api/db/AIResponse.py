df7c9fd4b254056125d2689e02107bedd2882255
Lucas-I-Marciano <lucas.marciano99@outlook.com>
2025-03-28 23:39:57 -0300

    refactor: Add foreign key relationship between AiResponse and AiResponseTags

--- backend/api/db/AIResponse.py
+++ backend/api/db/AIResponse.py
@@ -1,4 +1,5 @@
 from sqlmodel import SQLModel, Field
+from .AIResponseTags import AiResponseTags
 
 import datetime
 
@@ -6,4 +7,4 @@ class AiResponse(SQLModel, table=True):
     id: int | None = Field(primary_key=True, default=None)
     text: str
     sentiment_prediction: str
-    consulted_query_date: datetime.datetime = Field(index=True)
+    consulted_query_date: datetime.datetime = Field(index=True, foreign_key="airesponsetags.consulted_query_date")

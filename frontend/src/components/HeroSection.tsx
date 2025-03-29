28dc425b94ccf255ef812f8abbce61f1e74d7dcd
Pedro Oliveira <pedrogagodev@gmail.com>
2025-03-29 10:09:49 -0300

    feat: update HeroSection component with improved layout, typography, and responsive design for better user experience

--- frontend/src/components/HeroSection.tsx
+++ frontend/src/components/HeroSection.tsx
@@ -1,25 +1,40 @@
 import { AspectRatio } from '@/components/ui/aspect-ratio';
 import { Button } from '@/components/ui/button';
 import HeroImage from '@/assets/hero-image.png';
+import { ArrowRight } from 'lucide-react';
+
+// TODO: Fix responsive in AspectRatio component
 
 export function HeroSection() {
   return (
-    <div>
-      <h1 className="font-bold text-2xl mt-16 text-gray-800 mb-5 md:text-5xl">
-        Find out what people are saying about your brand{' '}
-        <span className="font-bold text-sky-700">in seconds.</span>
-      </h1>
-      <p className="text-gray-500 md:text-2xl">
-        Now you don't need to dedicate a team for weeks to find out what people
-        are saying about your brand, we do it for you in seconds.
-      </p>
+    <div className="bg-gray-100 flex flex-col items-center justify-center lg:items-start lg:text-start lg:w-full lg:max-h-[70vh] lg:px-40 lg:flex-row">
+      <div className="flex flex-col">
+        <h1 className="font-bold text-4xl mt-16 mb-5 md:text-6xl">
+          Understand customer feedback{' '}
+          <span className="font-bold text-sky-700">in seconds.</span>
+        </h1>
+        <p className="text-2xl text-gray-500 mt-8 lg:mt-16 md:text-2xl md:max-w-5xl">
+          FeedAI analyzes your customer reviews instantly. No more dedicating
+          teams for weeks to understand what people are saying about your brand.
+        </p>
+        <Button className="hidden lg:flex mt-32 mb-10 h-16 rounded-xl bg-sky-700 font-bold text-2xl text-white w-8/9 hover:bg-sky-800 hover:cursor-pointer">
+          Get Started{' '}
+          <ArrowRight className="ml-2 mt-[1px] min-w-8 min-h-8 font-bold" />
+        </Button>
+      </div>
 
-      <AspectRatio ratio={16 / 9} className="w-full p-10">
-        <img src={HeroImage} alt="Landing Page" className="rounded-lg" />
-      </AspectRatio>
-      <Button className="mt-16 w-1/2 bg-sky-700 text-white md:mt-32 md:py-6 md:w-1/3 md:text-xl">
-        Get Started
-      </Button>
+      <div className="flex flex-col w-full items-center gap-20">
+        <AspectRatio
+          ratio={16 / 9}
+          className="lg:max-h-[70vh] w-full p-10 2xl:mt-0"
+        >
+          <img src={HeroImage} alt="Landing Page" className="rounded-lg" />
+        </AspectRatio>
+
+        <Button className="mt-16 mb-10 w-10/11 h-12 py-7 bg-sky-700 font-bold text-lg text-white lg:hidden">
+          Get Started <ArrowRight className="w-5 h-5 font-bold mt-0.5" />
+        </Button>
+      </div>
     </div>
   );
 }

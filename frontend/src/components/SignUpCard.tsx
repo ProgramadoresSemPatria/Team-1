342c63ed460a1439d3e44efc977c00f8b37eff73
Pedro Oliveira <pedrogagodev@gmail.com>
2025-03-30 01:07:32 -0300

    refactor: streamline SignUpCard component structure and improve form handling

--- frontend/src/components/SignUpCard.tsx
+++ frontend/src/components/SignUpCard.tsx
@@ -1,90 +1,89 @@
-import { Card, CardContent, CardHeader, CardTitle } from "./ui/card";
-import { Stepper } from "./Stepper";
-import { accountInfoSchema, AccountInfoStep } from "./steps/AccountInfoStep";
+import { Card, CardContent, CardHeader, CardTitle } from './ui/card';
+import { Stepper } from './Stepper';
+import { accountInfoSchema, AccountInfoStep } from './steps/AccountInfoStep';
 import {
-	enterpriseInfoSchema,
-	EnterpriseInfoStep,
-} from "./steps/EnterpriseInfoStep";
-import { personalInfoSchema, PersonalInfoStep } from "./steps/PersonalInfoStep";
-import { z } from "zod";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { FormProvider, useForm } from "react-hook-form";
-import useAuthContext from "@/hooks/useAuth";
+  enterpriseInfoSchema,
+  EnterpriseInfoStep,
+} from './steps/EnterpriseInfoStep';
+import { personalInfoSchema, PersonalInfoStep } from './steps/PersonalInfoStep';
+import { z } from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { FormProvider, useForm } from 'react-hook-form';
+import useAuthContext from '@/hooks/useAuth';
 const schema = z.object({
-	personalInfo: personalInfoSchema,
-	enterpriseInfo: enterpriseInfoSchema,
-	accountInfo: accountInfoSchema,
+  personalInfo: personalInfoSchema,
+  enterpriseInfo: enterpriseInfoSchema,
+  accountInfo: accountInfoSchema,
 });
 
 export type SignUpFormData = z.infer<typeof schema>;
 export function SignUpCard() {
+  const form = useForm<SignUpFormData>({
+    resolver: zodResolver(schema),
+    defaultValues: {
+      personalInfo: {
+        name: '',
+        CPF: '',
+        birthdate: '',
+      },
+      enterpriseInfo: {
+        CNPJ: '',
+        enterpriseName: '',
+        businessType: '',
+      },
+      accountInfo: {
+        email: '',
+        password: '',
+        confirmPassword: '',
+      },
+    },
+  });
 
-	const form = useForm<SignUpFormData>({
-		resolver: zodResolver(schema),
-		defaultValues: {
-			personalInfo: {
-				name: "",
-				CPF: "",
-				birthdate: "",
-			},
-			enterpriseInfo: {
-				CNPJ: "",
-				enterpriseName: "",
-				businessType: "",
-			},
-			accountInfo: {
-				email: "",
-				password: "",
-				confirmPassword: "",
-			},
-		},
-	});
+  const { register } = useAuthContext();
 
-	const { register } = useAuthContext();
+  const handleSubmit = form.handleSubmit(async (data) => {
+    await register({
+      cnpj: data.enterpriseInfo.CNPJ,
+      company_name: data.enterpriseInfo.enterpriseName,
+      company_type: data.enterpriseInfo.businessType,
+      cpf: data.personalInfo.CPF,
+      email: data.accountInfo.email,
+      name: data.personalInfo.name,
+      password: data.accountInfo.password,
+      username: data.personalInfo.name,
+    });
+  });
 
-	const handleSubmit = form.handleSubmit(async (data) => {
-		await register({
-			cnpj: data.enterpriseInfo.CNPJ,
-			company_name: data.enterpriseInfo.enterpriseName,
-			company_type: data.enterpriseInfo.businessType,
-			cpf: data.personalInfo.CPF,
-			email: data.accountInfo.email,
-			name: data.personalInfo.name,
-			password: data.accountInfo.password,
-			username: data.personalInfo.name,
-		});
-	});
+  return (
+    <Card className="mx-26 mb-10 lg:min-w-[50vh] lg:p-6">
+      <CardHeader>
+        <CardTitle className="text-center text-xl lg:text-3xl">
+          Create your FeedAI account
+        </CardTitle>
+      </CardHeader>
+      <CardContent>
+        <FormProvider {...form}>
+          <form onSubmit={handleSubmit}>
+            <Stepper
+              steps={[
+                {
+                  label: 'Personal Information',
+                  content: <PersonalInfoStep />,
+                },
+                {
+                  label: 'Enterprise Information',
+                  content: <EnterpriseInfoStep />,
+                },
 
-	return (
-		<Card className="mx-26 lg:w-[50vh] lg:p-6">
-			<CardHeader>
-				<CardTitle className="text-center text-xl lg:text-3xl">
-					Create your FeedAI account
-				</CardTitle>
-			</CardHeader>
-			<CardContent>
-				<FormProvider {...form}>
-					<form onSubmit={handleSubmit}>
-						<Stepper
-							steps={[
-								{
-									label: "Personal Information",
-									content: <PersonalInfoStep />,
-								},
-								{
-									label: "Enterprise Information",
-									content: <EnterpriseInfoStep />,
-								},
-
-								{
-									label: "Account Information",
-									content: <AccountInfoStep />,
-								},
-							]}
-						/>
-					</form>
-				</FormProvider>
-			</CardContent>
-		</Card>
-	);
+                {
+                  label: 'Account Information',
+                  content: <AccountInfoStep />,
+                },
+              ]}
+            />
+          </form>
+        </FormProvider>
+      </CardContent>
+    </Card>
+  );
 }

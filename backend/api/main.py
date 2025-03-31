cb36c58378ecd4e83c3d40bc0a16875fdfe1d414
Lucas-I-Marciano <lucas.marciano99@outlook.com>
2025-03-31 20:58:13 -0300

    fix: DATABASE_URL .env

--- backend/api/main.py
+++ backend/api/main.py
@@ -53,5 +53,5 @@ def root(token: Annotated[protected_endpoint, Depends()]):
     return {"Message": "Hello World!"}
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8000))
+    port = int(os.getenv("PORT", 8000))
     uvicorn.run("main:app", host="0.0.0.0", port=port)

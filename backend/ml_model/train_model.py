a89d3a0452089beab3215b640d3ce11a53c7622f
Pedro Oliveira <pedrogagodev@gmail.com>
2025-03-31 20:19:42 -0300

    feat: enhance model

--- backend/ml_model/train_model.py
+++ backend/ml_model/train_model.py
@@ -1,10 +1,10 @@
 import pandas as pd 
 from sklearn.model_selection import train_test_split
-from sklearn.naive_bayes import MultinomialNB
 from sklearn.metrics import classification_report
 from sklearn.feature_extraction.text import CountVectorizer
 import joblib
 import os 
+from sklearn.svm import LinearSVC
 df = pd.read_csv('dataset/reduced_reviews.csv')
 
 vectorizer = CountVectorizer(max_features=5000)
@@ -17,7 +17,7 @@ X_train, X_test , y_train, y_test = train_test_split(
     X, y, test_size= 0.3, random_state=42
 )
 
-model = MultinomialNB()
+model = LinearSVC(dual=False)
 model.fit(X_train,y_train)
 
 y_pred = model.predict(X_test)

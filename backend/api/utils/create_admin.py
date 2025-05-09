94cce23056f9ea6e080f0698031d8c5237691509
Lucas Ioran Marciano <lucas.marciano99@outlook.com>
2025-05-09 20:46:52 -0300

    refactor: update create_admin function to include error handling and improve documentation

--- backend/api/utils/create_admin.py
+++ backend/api/utils/create_admin.py
@@ -8,11 +8,16 @@ load_dotenv()
 from api.db import engine
 from api.db.Users import Users
 from api.utils.token import create_hash_password
+from api.utils.exception_handler import handle_exception
 
 ADMIN_PASSWORD = os.getenv('ADMIN_PASSWORD')
 
-def create_admin():
-    admin_user = {
+def create_admin() -> None:
+    """Cria um usuário administrador no banco de dados.
+
+    Verifica se o usuário administrador já existe e, se não, cria um novo.
+    """
+    admin_user_data = {
         "name": "admin",
         "username": "admin",
         "email": "admin@admin.com",
@@ -23,18 +28,23 @@ def create_admin():
         "password": ADMIN_PASSWORD,
         "is_admin": True
     }
+
+    if not ADMIN_PASSWORD:
+        raise ValueError("ADMIN_PASSWORD não está definido no ambiente.")
+
     with Session(engine) as session:
         try:
-            user_to_db = Users.model_validate(admin_user)
-            user_to_db.password = create_hash_password(admin_user.get('password'))
+            user_to_db = Users.model_validate(admin_user_data)
+            user_to_db.password = create_hash_password(admin_user_data.get('password'))
 
             statement = select(Users.email).where(Users.email == "admin@admin.com")
             admin_email = session.execute(statement).one_or_none()
             if admin_email:
+                print("O usuário administrador já existe.")
                 return
 
             session.add(user_to_db)
             session.commit()
             session.refresh(user_to_db)
-        except Exception as e :
-            print(e)
+        except Exception as e:
+            handle_exception(e)
